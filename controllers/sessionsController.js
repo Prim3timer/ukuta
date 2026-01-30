56f754479f71d665865e5aaeebab3b5a03d65d6d
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2026-01-30 00:13:50 -0500

    removed shipping address

--- controllers/sessionsController.js
+++ controllers/sessionsController.js
@@ -51,9 +51,9 @@ const makePayment = async (req, res) => {
                 }
 
             }),
-            shipping_address_collection: {
-                allowed_countries: ['US', 'NG']
-            },
+            // shipping_address_collection: {
+            //     allowed_countries: ['US', 'NG']
+            // },
 
             success_url: `${process.env.CLIENT_URL}/#cart/thanks?session_id={CHECKOUT_SESSION_ID}`,
             cancel_url: `${process.env.CLIENT_URL}/shopping`,
