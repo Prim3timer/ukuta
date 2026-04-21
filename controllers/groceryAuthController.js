5064d8925ec449d5921c33e5d24f46b7d970e19b
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2026-04-21 08:52:46 -0400

    refresh and accessTokens

--- controllers/groceryAuthController.js
+++ controllers/groceryAuthController.js
@@ -28,7 +28,7 @@ const handleLogin = asyncHandler(async (req, res) => {
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "10s" },
+      { expiresIn: "15m" },
     );
     const refreshToken = jwt.sign(
       { username: foundUser.username },
