b56249ed47ec7ac1fab53ae515ea57f21bd57ec6
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2026-07-14 16:04:51 -0400

    remove shipping address

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -4,33 +4,6 @@ const itemsController = require("../controllers/itemsController");
 const multer = require("multer");
 const fs = require("fs");
 
-// const storage = multer.diskStorage({
-//   destination: async (req, file, cb) => {
-//     const { name, unitMeasure, price, image, now } = req.body;
-//     // const {name} = req.params
-//     const files = req.files;
-//     console.log({ files });
-//     console.log({ name });
-//     // console.log({fileO: req.files})
-//     if (!fs.existsSync(path.join(__dirname, "public", "images", `./${name}`))) {
-//       await fs.promises.mkdir(
-//         path.join(__dirname, "public", "images", `./${name}`),
-//       );
-//       cb(null, `./public/images/${name}`);
-//       console.log(`./${name} created`);
-//     } else cb(null, `./public/images/${name}`);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const upload = multer({
-//   storage,
-// });
-
-// const uploadMultiple = upload.array('images', 5)
-
 router.route("/").get(itemsController.getAllItems);
 
 router.route("/").post(itemsController.createNewItem);
